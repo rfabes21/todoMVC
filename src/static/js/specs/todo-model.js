5685118d1ea384b25612b8aa6c24080df1d1d37f
Ryan <rfabester@gmail.com>
2014-07-16 10:16:28 -0700

    added spy unit test

--- src/static/js/specs/todo-model.js
+++ src/static/js/specs/todo-model.js
@@ -34,6 +34,31 @@ describe('Todo Model', function() {
         expect(todo.toJSON().completed).toBe(true);
     });
 
+    it('should succeed', function() {
+        var spy = spyOn(ItemView.prototype, 'wantsRemove').and.callThrough();
+
+        var collection = new backbone.Collection();
+        var myView = new CompositeView({collection: collection});
+
+        region.show(myView);
+        collection.add({label: 'Hi Aubrey'});
+        collection.add({label: 'Hi Ryan'});
+
+        var kids = $('.item .actions');
+        var view = myView.children.findByIndex(0);
+
+        expect(view.model.get('label')).toBe('Hi Aubrey');
+
+        kids.eq(0).trigger('click');
+
+        expect(spy.calls.count()).toBe(1);
+        expect(spy).toHaveBeenCalled();
+
+        view = myView.children.findByIndex(0);
+        expect(view.model.get('label')).toBe('Hi Ryan');
+    });
+
+
 
 }); // Eof describe
 }); // Eof define
